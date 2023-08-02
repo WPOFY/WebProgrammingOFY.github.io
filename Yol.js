2730c9a5d3ebecdda65e8b964e802f8b6786ff5d
WPOFY <webprogrammingOFY@gmail.com>
2023-08-02 18:44:21 +0700

    first commit

---
+++ Yol.js
@@ -0,0 +1,10 @@
+function func(){
+    var Name = document.getElementById("Name").value;
+    if (Name == "Jessy Yolanda") {
+        alert("Succesfull ! ")
+        window.location = "Yol 2.html";
+    }
+    else {
+        alert("Unidentified ! ")
+    }
+}
